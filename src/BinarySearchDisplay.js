e05cea55a60c22c16ef571b2ae4e5183c331eec7
Sakuya-wd <yamamoto1.527@gmail.com>
2020-09-09 10:53:48 +0900

    git push時に自動でコードを整形する

--- src/BinarySearchDisplay.js
+++ src/BinarySearchDisplay.js
@@ -1,19 +1,15 @@
+//git commit時にeslintで構文チェックしprettierでコードフォーマットを修正する。
 
 const BinarySearchConfirm = require("./BinarySearchConfirm.js");
 
+const BinarySearchDisplay = () => {
+  //検索する値(正の整数)。
+  let search = 1;
+  let result = BinarySearchConfirm(search);
 
-const BinarySearchDisplay = () =>{
-
-   //検索する値(正の整数)。
-   let search = 1;
-   let result = BinarySearchConfirm(search);
-
-   console.log(result);
-
-}
-
+  console.log(result);
+};
 
 BinarySearchDisplay();
 
 module.exports = BinarySearchDisplay;
-
